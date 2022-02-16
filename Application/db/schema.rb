56755277688c847e07701ee812cb9142d84421a0
anukhatri41 <anukhatri41@gmail.com>
2022-02-15 18:30:32 -0600

    Update storage.yml

--- Application/db/schema.rb
+++ Application/db/schema.rb
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2022_03_03_201434) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "members", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "first"
+    t.string "last"
+    t.string "organizations"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.integer "price"
@@ -52,23 +62,13 @@ ActiveRecord::Schema.define(version: 2022_03_03_201434) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "first"
-    t.string "last"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "members", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
     t.string "first"
     t.string "last"
-    t.string "organizations"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
